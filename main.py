37af1fa36bf112bcb95361b8b0c025a20a5c151c
PrabhuMMoorthy <mprabhumoorthy@gmail.com>
2017-08-28 13:43:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
-import os
+#!/usr/bin/env python
+
+from tornado.options import options, define, parse_command_line
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.wsgi
+import tornado.websocket
+import json
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello world")
+define('port', type=int, default=9001)
+
+class HelloHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write('Hello from tornado')
+
+class MyWebSocket(tornado.websocket.WebSocketHandler):
+  clients = []
+
+  def check_origin(self, origin):
+    return True
+
+  def open(self):
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.append(self)
+    print("WebSocket opened")
+
+  def on_message(self, message):
+    print("msg recevied", message)
+    msg = json.loads(message) # todo: safety?
+
+    # send other clients this message
+    for c in MyWebSocket.clients:
+      if c != self:
+        c.write_message(msg)
+
+  def on_close(self):
+    print("WebSocket closed")
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.remove(self)
 
 def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
-    http_server.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
+  tornado_app = tornado.web.Application([
+      ('/hello-tornado', HelloHandler),
+      ('/', MyWebSocket),
+      ])
+  server = tornado.httpserver.HTTPServer(tornado_app)
+  server.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
